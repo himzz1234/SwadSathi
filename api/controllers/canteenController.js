a8b550a01df065c7ef2d8e1e90dfe7e18d66b83c
Akshay <akshaybhisikar99@gmail.com>
2023-10-01 15:34:55 +0530

    Get details of logged In user

--- api/controllers/canteenController.js
+++ api/controllers/canteenController.js
@@ -6,6 +6,12 @@ const Item = require("../models/Item");
 const dotenv = require("dotenv");
 const generateToken = require("../utils/generateToken");
 const cloudinary = require('cloudinary').v2
+
+cloudinary.config({
+  cloud_name: process.env.CLOUD_NAME,
+  api_key: process.env.API_KEY,
+  api_secret: process.env.API_SECRET
+})
 // @desc Canteen Login
 // @route POST canteen/login
 const register = async (req, res) => {
