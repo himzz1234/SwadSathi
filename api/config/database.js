51a6521f0c57e9acfc9604d03824b1eef17d35c5
Akshay <akshaybhisikar99@gmail.com>
2023-10-29 13:14:35 +0530

    socket io implementation

--- api/config/database.js
+++ api/config/database.js
@@ -1,5 +1,6 @@
 const mongoose = require('mongoose');
 const dotenv = require('dotenv');
+
 dotenv.config();
 const MONGO_URI = process.env.MONGO_URI;
 
--- api/server.js
+++ api/server.js
@@ -3,12 +3,43 @@ const cookieParser = require("cookie-parser");
 const connectToMongo = require("./config/database.js");
 const cors = require("cors");
 const app = express();
+const server = require("http").createServer(app)
 const port = 8800;
+
+const io = require("socket.io")(server, {
+  perMessageDeflate: false,
+  cors: {
+    origin: "*",
+  }
+})
 
 connectToMongo();
 app.use(cors());
 app.use(express.json());
 app.use(cookieParser());
+
+let connectedUsers = []
+let connectedCanteens = []
+
+const addnewuser = (userId, socketId) => {
+  !connectedUsers.some((user)=>user.userId === userId) && connectedUsers.push({userId, socketId})
+}
+
+const addNewCanteen = (canteenId, socketId) => {
+  !connectedCanteens.some((canteen)=>canteen.canteenId === canteenId) && connectedCanteens.push({canteenId, socketId})
+}
+
+const getUser = (userId) => {
+  return connectedUsers.find((user)=>user.userId === userId)
+}
+
+io.on("connection", (socket)=>{
+  socket.on("newuser", (userId)=>{
+    addnewuser(userId, socket.id)
+  })
+  
+  socket.on("disconnect", ()=>{})
+})
 
 //app.use('/api/items',require('./routes/foodItemRoute'));
 app.use("/api/auth/user", require("./routes/userRoutes"));
